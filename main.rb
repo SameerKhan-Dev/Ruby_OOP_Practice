917345746079ead9e109069af18c89ca50c41737
Sameer Khan <sameerk_786@hotmail.com>
2021-02-05 22:52:07 +0000

    added player class and instantiated inside main.rb

---
+++ main.rb
@@ -0,0 +1,15 @@
+
+# First lets require the files
+
+require './player'
+#require 'questionAnswer'
+
+# instantiate two player objects
+
+playerOne = Player.new(1)
+playerTwo = Player.new(2)
+
+# Lets test out that the players are being instantiated properly
+p playerOne
+p playerTwo
+
---
+++ player.rb
@@ -0,0 +1,8 @@
+
+class Player
+  attr_accessor :id
+  def initialize(id)
+    @id = id
+    @score = 3
+  end
+end
